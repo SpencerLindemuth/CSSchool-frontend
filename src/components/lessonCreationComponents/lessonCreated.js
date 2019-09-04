03534849772890f7c93afb454197ae386fbec1ef
Spencer Lindemuth <lindemut@gmail.com>
2019-09-04 11:19:53 -0700

    Format share link page

--- src/components/lessonCreationComponents/lessonCreated.js
+++ src/components/lessonCreationComponents/lessonCreated.js
@@ -6,6 +6,8 @@ export default class LessonCreated extends React.Component{
     render(){
         return(
             <div id="created">
+                <h3>Your lesson has been created! You can view it or share it with this link:</h3>
+                <br />
                 <Link to={`lesson/${this.props.lessonId}`}>{`localhost:3000/lesson/${this.props.lessonId}`}</Link>
             </div>
         )
