0353141988524643e480bb1741afc03c646b3635
Spencer Lindemuth <lindemut@gmail.com>
2019-09-11 12:00:20 -0700

    Unescape tabs and newlines

--- src/components/lessonComponents/codeView.js
+++ src/components/lessonComponents/codeView.js
@@ -3,11 +3,19 @@ import ReactMarkdown from 'react-markdown/with-html'
 
 export default class CodeView extends React.Component {
 
+    stripEscapes = () => {
+        console.log(this.props.code)
+        let x = String.raw`${this.props.code}`
+        let y = x.replace("\\n", "\n" )
+        let z = y.replace("\\v", "\v")
+        return z.replace("\\t", "\t")
+    }
 
     render(){
         return(
             <div id="codeview" className="game">
-                <ReactMarkdown source={this.props.code} escapeHtml={false} />
+                {console.log(this.props.code)}
+                <ReactMarkdown source={this.stripEscapes()} escapeHtml={false} />
             </div>
         )
     }
